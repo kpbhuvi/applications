6b7311846748f35d8998d80295c19dbec6e46f83
BHUVI\bhuvana <kpbhuv@gmail.com>
2022-10-19 17:01:56 -0400

    second commit

---
+++ python/formatted_str.py
@@ -0,0 +1,2 @@
+first ='john'
+last = 'smith'
---
+++ python/ip.py
@@ -0,0 +1,8 @@
+#name = input('what is your name ')
+#print('Hi'+ name)
+
+
+person_name = input('what is your name? ')
+color = input('what is your favourite color ')
+
+print(person_name + 'likes' + color)
